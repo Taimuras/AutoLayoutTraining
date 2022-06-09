9063ad2a17921d38600eef34ac1c6ea32b381054
Timur <avelonadreamer@gmail.com>
2022-06-09 19:13:22 +0600

    First push

---
+++ PhotoTestApp/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  PhotoTestApp
+//
+//  Created by Timur on 7/6/22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoTestApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PhotoTestApp
+//
+//  Created by Timur on 7/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        self.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
+
+}
